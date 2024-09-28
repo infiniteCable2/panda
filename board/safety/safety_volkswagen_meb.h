5adb6a40bb7a234a2719136ef3b9b53553d4a86f
infiniteCable2 <steffen.dahansi.werner@googlemail.com>
2024-09-28 12:48:59 +0200

    Update safety_volkswagen_meb.h testing getting car limits

--- board/safety/safety_volkswagen_meb.h
+++ board/safety/safety_volkswagen_meb.h
@@ -24,11 +24,11 @@ const SteeringLimits VOLKSWAGEN_MEB_STEERING_LIMITS = {
   .angle_deg_to_can = 5747, // (1 / 0.0174) * 100 deg to can (minimize rounding error)
   .angle_rate_up_lookup = {
     {0., 8., 15.},
-    {2000., 1000., 60.} //{1200., 400., 60.}
+    {9000., 9000., 9000.} //{1200., 400., 60.}
   },
   .angle_rate_down_lookup = {
     {0., 8., 15.},
-    {2000., 1200., 80.} //{1200., 600., 80.}
+    {9000., 9000., 9000.} //{1200., 600., 80.}
   },
   .inactive_angle_is_zero = true,
 };
